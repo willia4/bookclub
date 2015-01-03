b6ea2551353c3df77f2cb68e84f08c66aefd8d76
willia4 <james@jameswilliams.me>
2015-01-02 19:17:01 -0500

    Add support for importing scss files. This was surprisingly easy to do.

--- app.rb
+++ app.rb
@@ -3,31 +3,30 @@ require './database.rb'
 
 # The sass rack plugin just won't work (it refuses to update the css if you change the .scss template and ignores its configuration and is generally horrible)
 # so this will let us at least use sass css files without having to run compass or something. 
-# The biggest downside of this is that @import won't work. I guess I could hack that in if necessary, but I'll probably just change it 
-# to use compass if we ever need that. 
 #
 # I really hate open source sometimes. 
 get '/stylesheets/:name.css' do |name|
-  css_path = "./public/stylesheets/#{name}.css"
-  scss_path = "./public/stylesheets/sass/#{name}.scss"
+  css_path = File.join($config[:css][:css_path], "#{name}.css")
+  scss_path = File.join($config[:css][:scss_path], "#{name}.scss")
+  
+  halt(404) if not File.exist?(css_path) and not File.exist?(scss_path)
+
+  content_type 'text/css'
 
   if File.exist?(css_path)
-    content_type 'text/css'
-    content = File.open(css_path, 'r') { |file| file.read }
-
-    content
+    return File.open(css_path, 'r') { |file| file.read }
   else
-    halt(404) if not File.exist?(scss_path)
-
-    content_type 'text/css'
     content = File.open(scss_path, 'r') { |file| file.read }
 
     engine = Sass::Engine.new content, :syntax => :scss
-    engine.render  
+    return engine.render  
   end  
 end
 
 configure do 
+  # make sure that sass knows where to find includes
+  Sass.load_paths << $config[:css][:scss_path]
+
   #Database.init_amazon
 end
 
