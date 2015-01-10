e9aec974c98287ff5950905629ca1c8af929a336
willia4 <james@jameswilliams.me>
2015-01-10 14:58:54 -0500

    Add some code to make it easier to debug xsrf issues

--- database/debug.rb
+++ database/debug.rb
@@ -23,6 +23,22 @@ module Database
       sessions
     end
 
+    def self.list_all_xsrf_tokens
+      query = "select * from #{Database::SDB.build_domain("xsrf_tokens")}"
+      data = Database::SDB.get_database_client.select(select_expression: query)
+      tokens = []
+      data.each do |page|
+        tokens.concat(page.data.items.map do |item|
+          {
+            xsrf_token: Database::SDB.find_attribute(item, "token"),
+            expires: Database::SDB.find_attribute(item, "expires"),
+            action_type: Database::SDB.find_attribute(item, "action_type")
+          }
+        end)
+      end
+      tokens
+    end
+
     def self.delete_all_profiles
       profiles = Database::UserProfiles.list_user_profiles
       Database::SDB.delete_items('profiles', profiles.map { |p| p.user_id })
