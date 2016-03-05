willia4/bookclub

commit 0958fd504504ce5520412c166bc8b73da205696f

Resolves #42 - turns out this is easier than Stack Overflow made it out to be as long as you aren't wedded to a custom s3 domain

src/routes/login.rb
@@ -48,10 +48,10 @@ get '/signin/facebook/finish' do
   facebook_id = info["bio"]["id"]
 
   #look up the user to see if they already exist
-  profile = Database::UserProfiles.find_user_profile_by_facebook_id(facebook_id)
+  profile = Database::UserProfiles.find_user_profile_by_facebook_id(request, facebook_id)
   
   if profile.nil? 
-    admin_profiles = Database::UserProfiles.list_admin_user_profiles 
+    admin_profiles = Database::UserProfiles.list_admin_user_profiles(request)
 
     profile = Models::UserProfile.new
     profile.user_status = "unconfirmed"

src/database/books.rb
@@ -56,50 +56,50 @@ module Database
       end
     end
 
-    def self.list_books
-      return list_books_from_sdb_query("select * from #{SDB.build_domain("books")}")
+    def self.list_books(request)
+      return list_books_from_sdb_query(request, "select * from #{SDB.build_domain("books")}")
     end
 
-    def self.list_unread_books
-      return list_books_from_sdb_query("select * from #{SDB.build_domain("books")} where read = 'false' and (rejected is null or rejected = 'false') ")
+    def self.list_unread_books(request)
+      return list_books_from_sdb_query(request, "select * from #{SDB.build_domain("books")} where read = 'false' and (rejected is null or rejected = 'false') ")
     end
 
-    def self.list_rejected_books
-      return list_books_from_sdb_query("select * from #{SDB.build_domain("books")} where rejected = 'true'")
+    def self.list_rejected_books(request)
+      return list_books_from_sdb_query(request, "select * from #{SDB.build_domain("books")} where rejected = 'true'")
     end
 
-    def self.list_read_books
-      return list_books_from_sdb_query("select * from #{SDB.build_domain("books")} where read = 'true'")
+    def self.list_read_books(request)
+      return list_books_from_sdb_query(request, "select * from #{SDB.build_domain("books")} where read = 'true'")
     end
 
-    def self.find_book_by_book_id(book_id)
-      book = Redis.find_book_by_book_id(book_id)
+    def self.find_book_by_book_id(request, book_id)
+      book = Redis.find_book_by_book_id(request, book_id)
       return book if !book.nil?
 
       item = Database::SDB.find_first_item_by_attribute("books", "book_id", book_id)
       return nil if item.nil?
 
-      book = build_book_from_sdb_item(item)
+      book = build_book_from_sdb_item(request, item)
       Redis.store_book(book)
       return book
     end
 
-    def self.list_books_from_sdb_query query
+    def self.list_books_from_sdb_query(request, query)
       data = SDB.select(query)
 
       books = []
       data.each do |page|
-        books.concat(page.data.items.map { |i| build_book_from_sdb_item(i) })
+        books.concat(page.data.items.map { |i| build_book_from_sdb_item(request, i) })
       end
 
       books.each { |b| Redis.store_book(b) }
       return books
     end
 
-    def self.build_book_from_sdb_item(item)
+    def self.build_book_from_sdb_item(request, item)
       return nil if item.nil?
 
-      book = Models::Book.new
+      book = Models::Book.new(request)
       Models::Book.sdb_properties.each do |p|
         value = SDB.find_attribute(item, p).to_s
         method = p + "="

src/database/debug.rb
@@ -41,13 +41,13 @@ module Database
       tokens
     end
 
-    def self.delete_all_user_profiles
-      profiles = Database::UserProfiles.list_user_profiles
+    def self.delete_all_user_profiles(request)
+      profiles = Database::UserProfiles.list_user_profiles(request)
       Database::SDB.delete_items('profiles', profiles.map { |p| p.user_id })
     end
 
-    def self.delete_all_books
-      books = Database::Books.list_books
+    def self.delete_all_books(request)
+      books = Database::Books.list_books(request)
       Redis.delete_all_books
       Database::SDB.delete_items('books', books.map { |b| b.book_id })
     end

src/database/redis.rb
@@ -52,7 +52,7 @@ module Database
       redis.expire(key, expiration_time_in_seconds)
     end
 
-    def self.find_user_profile_for_session(session_token)
+    def self.find_user_profile_for_session(request, session_token)
       redis = get_database_client
 
       pattern = "session:#{session_token}:*"
@@ -61,7 +61,7 @@ module Database
       if keys.size > 0 
         key = keys[0]
         json = redis.get(key)
-        return Models::UserProfile.from_json(json)
+        return Models::UserProfile.from_json(request, json)
       end
 
       return nil
@@ -89,11 +89,11 @@ module Database
       redis.expire(key, expiration_time_in_seconds)
     end
 
-    def self.find_book_by_book_id(book_id)
+    def self.find_book_by_book_id(request, book_id)
       redis = get_database_client
       json = redis.get("book:#{book_id}")
       return nil if json.nil? 
-      return Models::Book.from_json(json)
+      return Models::Book.from_json(request, json)
     end
 
     def self.delete_book_id(book_id)

src/database/s3.rb
@@ -113,5 +113,35 @@ module Database
         true
       end
     end
+
+    def self.fixup_s3_url_for_https(scheme, s3_url)
+      #this is gross, but it avoids having to migrate data in SDB. 
+      #because our bucketnames may have dots in them, we can't use https://bucket.name.s3.amazonaws.com
+      #(amazon's wildcard TLS sert only works for bucket names without dots).
+      #so we need to use the https://s3.amazonaws.com/bucket.name/key form of the url to get a valid
+      #https connection. But we've been storing the first form in SDB for months. 
+      #fortunately, rewriting the stored URL is fairly easy...
+
+      url = URI(s3_url)
+
+      if url.host.include?("s3.amazonaws.com")
+        bucket = url.host 
+        bucket.slice!(".s3.amazonaws.com")
+        bucket = "/#{bucket}/" #URI.join needs the middle portions of a path to be surrounded by "/"s
+
+        key = url.path 
+        key.slice!("/")        #the path comes in with a "/" but URI.join needs the end portion to not have a "/"
+
+        url = URI("http://s3.amazonaws.com")
+        url = URI.join(url, bucket, key)
+        url.scheme = scheme
+      else
+        #if this doesn't look like an S3 url, just fix the scheme
+        url.scheme = scheme
+      end
+
+      return url.to_s
+    end
+
   end
 end

src/database/user_profile.rb
@@ -5,14 +5,14 @@ require './models/user_profile.rb'
 
 module Database
   module UserProfiles
-    def self.find_user_profile_by_user_id user_id
+    def self.find_user_profile_by_user_id(request, user_id)
       item = Database::SDB.find_first_item_by_attribute("profiles", "user_id", user_id)
-      return build_profile_from_sdb_item(item)
+      return build_profile_from_sdb_item(request, item)
     end
 
-    def self.find_user_profile_by_facebook_id facebook_id 
+    def self.find_user_profile_by_facebook_id(request, facebook_id) 
       item = Database::SDB.find_first_item_by_attribute("profiles", "facebook_id", facebook_id)
-      return build_profile_from_sdb_item(item)
+      return build_profile_from_sdb_item(request, item)
     end
 
     def self.save_user_profile profile 
@@ -55,34 +55,34 @@ module Database
       return 0
     end
 
-    def self.list_user_profiles
+    def self.list_user_profiles(request)
       query = "select * from #{SDB.build_domain("profiles")}"
       data = SDB.select(query)
 
       profiles = []
       data.each do |page|
-        profiles.concat page.data.items.map { |i| build_profile_from_sdb_item(i) }
+        profiles.concat page.data.items.map { |i| build_profile_from_sdb_item(request, i) }
       end
 
       return profiles
     end
 
-    def self.list_admin_user_profiles
+    def self.list_admin_user_profiles(request)
       query = "select * from #{SDB.build_domain("profiles")} where user_status = 'admin'"
       data = SDB.select(query)
 
       profiles = []
       data.each do |page|
-        profiles.concat page.data.items.map { |i| build_profile_from_sdb_item(i) }
+        profiles.concat page.data.items.map { |i| build_profile_from_sdb_item(request, i) }
       end
 
       return profiles
     end
 
-    def self.build_profile_from_sdb_item item
+    def self.build_profile_from_sdb_item(request, item)
       return nil if item.nil? 
 
-      profile = Models::UserProfile.new
+      profile = Models::UserProfile.new(request)
 
       Models::UserProfile.profile_properties.each do |p|
         value = SDB.find_attribute(item, p).to_s

src/database/user_sessions.rb
@@ -26,9 +26,9 @@ module Database
     end
 
     # Returns a profile if the session is valid or nil if it is not
-    def self.validate_user_session session_token
+    def self.validate_user_session(request, session_token)
       #check redis first 
-      profile = Redis.find_user_profile_for_session(session_token)
+      profile = Redis.find_user_profile_for_session(request, session_token)
       return profile if profile 
 
       #clean up when validating just to keep the domain tidy
@@ -43,7 +43,7 @@ module Database
       user_id = SDB.find_attribute(item, "user_id")
       return nil if user_id.nil? 
 
-      profile = Database::UserProfiles.find_user_profile_by_user_id(user_id)
+      profile = Database::UserProfiles.find_user_profile_by_user_id(request, user_id)
 
       #if we found the session in SDB, we should save it in redis for next time 
       Redis.store_session(session_token, profile)

src/models/book.rb
@@ -12,7 +12,7 @@ module Models
     attr_accessor :author
     attr_accessor :summary_key
     attr_accessor :summary
-    attr_accessor :image_url
+    attr_writer :image_url
     attr_accessor :external_url
     attr_accessor :read
     attr_accessor :rejected
@@ -21,6 +21,11 @@ module Models
     #these are not filled in when loading a book but are there for the convenience of other methods
     attr_accessor :votes
     attr_accessor :user_vote
+
+    def initialize(request)
+      @scheme = request.scheme
+      @date_added = Time.now
+    end
 
     def read=(read)
       @read = ((read == "true") || (read == "yes") || (read == true))
@@ -56,12 +61,12 @@ module Models
       time_ago_in_words(@date_added)
     end
 
-    def book_url(request)
-      URI($config.make_url(request, "/books/book/#{self.book_id}"))
+    def image_url
+      Database::S3.fixup_s3_url_for_https(@scheme, @image_url)
     end
 
-    def initialize
-      @date_added = Time.now
+    def book_url(request)
+      URI($config.make_url(request, "/books/book/#{self.book_id}"))
     end
 
     def == other
@@ -86,8 +91,8 @@ module Models
       h.to_json
     end
 
-    def self.from_json(json_string)
-      r = Book.new
+    def self.from_json(request, json_string)
+      r = Book.new(request)
       h = JSON.parse(json_string)
 
       defaults = {"rejected" => "false", "addedby_id" => ""}

commit 7ca142a320fb4db31390ffb6600ddecb51301208

Resolve an issue that was introduced by commit 0958fd504504ce5520412c166bc8b73da205696f that prevented new users from being created.
Oops.

src/routes/login.rb
@@ -53,7 +53,7 @@ get '/signin/facebook/finish' do
   if profile.nil? 
     admin_profiles = Database::UserProfiles.list_admin_user_profiles(request)
 
-    profile = Models::UserProfile.new
+    profile = Models::UserProfile.new(request)
     profile.user_status = "unconfirmed"
 
     profile.full_name = info["bio"]["name"]